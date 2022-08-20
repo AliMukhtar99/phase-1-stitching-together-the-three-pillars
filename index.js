e9cf545d53551906928dfa5e97c0c5fa77d110bf
AliMukhtar99 <ali.mukhta@student.moringaschool.com>
2022-08-20 10:46:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 /* This is a file inserted to keep the learn IDE browser happy */
-const testVar = {}
+ const testVar = {}
+
+  
+  const articleHearts = document.querySelectorAll(".like-glyph");
+  
+  function likeCallback(e) {
+    const heart = e.target;
+    mimicServerCall()
+      .then(function (serverMessage) {
+        alert("You notified the server!");
+        alert(serverMessage);
+        heart.innerText = glyphStates[heart.innerText];
+        heart.style.color = colorStates[heart.style.color];
+      })
+      .catch(function (error) {
+        alert("Something went wrong!");
+      });
+  }
+  for (const glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+  }
